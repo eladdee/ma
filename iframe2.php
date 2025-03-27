d8aa9dbd9285c657c589377cb6c8ff77f1bb72fd
ddd <eladevrim65@gmail.com>
2025-03-27 14:02:50 +0300

    gadf
    adfg

--- iframe2.php
+++ iframe2.php
@@ -2006,7 +2006,7 @@
                         <div class="OM-Slider OM-Slider--ItemFullUnequalWidth">
                            <div class="OM-Slider__Bar">
                               <div class="OM-Slider__Scroll">
-                                 <div class="OM-Slider__Container" style="transform: translateX(0px);">
+                                 <div class="OM-Slider__Container" style="transform: translateX(-180px);">
                                     <div class="OM-Slider__Item"><span class="OM-WidgetsLink OM-WidgetsLink--NonAnchorLink OM-NavItem isSelected"><span class="OM-NavItem__Name">Futbol</span></span></div>
                                     <div class="OM-Slider__Item"><span class="OM-WidgetsLink OM-WidgetsLink--NonAnchorLink OM-NavItem"><span class="OM-NavItem__Name">Basketbol</span></span></div>
                                     <div class="OM-Slider__Item"><span class="OM-WidgetsLink OM-WidgetsLink--NonAnchorLink OM-NavItem"><span class="OM-NavItem__Name">Tenis</span></span></div>
